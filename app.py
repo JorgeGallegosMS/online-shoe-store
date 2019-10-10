3e83411ed74b8ac8208a98d927b1cc6580d3be7e
Jorge Gallegos <gallegosjorge403@gmail.com>
2019-10-10 11:59:57 -0700

    Able to update and delete shoes

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from bson.objectid import ObjectId
 host = os.environ.get('MONGODB_URI')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
+# REMOVE
 # db.shoes.drop()
 
 shoes = db.shoes
@@ -20,14 +21,16 @@ def shoes_index():
 @app.route('/shoes/new')
 def shoes_new():
     """Create a new shoe"""
-    return render_template('shoes_new.html.j2', shoe={})
+    return render_template('shoes_new.html.j2', shoe={}, title='Add Shoe')
 
 @app.route('/shoes', methods=['POST'])
 def shoes_submit():
     """Submit new shoe"""
     shoe = {
         'name': request.form.get('name'),
-        'price': request.form.get('price')
+        'price': request.form.get('price'),
+        'image_url': request.form.get('image_url'),
+        'description': request.form.get('description')
     }
     print(shoe)
     shoe_id = shoes.insert_one(shoe).inserted_id
@@ -38,3 +41,30 @@ def shoes_show(shoe_id):
     """Show an individual shoe"""
     shoe = shoes.find_one({'_id': ObjectId(shoe_id)})
     return render_template('shoes_show.html.j2', shoe=shoe)
+
+@app.route('/shoes/<shoe_id>/edit')
+def shoes_edit(shoe_id):
+    """Display the edit form"""
+    shoe = shoes.find_one({'_id': ObjectId(shoe_id)})
+    return render_template('shoes_edit.html.j2', shoe=shoe, title='Update Shoe')
+
+@app.route('/shoes/<shoe_id>', methods=['POST'])
+def shoes_update(shoe_id):
+    """Update shoe"""
+    updated_shoe = {
+        'name': request.form.get('name'),
+        'price': request.form.get('price'),
+        'image_url': request.form.get('image_url'),
+        'description': request.form.get('description')
+    }
+    shoes.update_one(
+        {'_id': ObjectId(shoe_id)},
+        {'$set': updated_shoe}
+    )
+    return redirect(url_for('shoes_show', shoe_id=shoe_id))
+
+@app.route('/shoes/<shoe_id>/delete', methods=['POST'])
+def shoes_delete(shoe_id):
+    """Remove a shoe"""
+    shoes.delete_one({'_id': ObjectId(shoe_id)})
+    return redirect(url_for('shoes_index'))
